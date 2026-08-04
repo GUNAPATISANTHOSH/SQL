75c80189b7cc647f8cca400347e927490042ed4b
GUNAPATISANTHOSH <santoshsantutej@gmail.com>
2026-08-04 12:10:57 +0530

    day-4

---
+++ Day-4.sql
@@ -0,0 +1,33 @@
+create table dept(
+dept_id int,
+dept_name VARCHAR(32) NOT NULL,
+dept_loc VARCHAR(32) default "bng",
+dept_budget float check(dept_budget >= 500000),
+PRIMARY KEY(dept_id)
+);
+
+create table employees(
+eid INT,
+ename VARCHAR(32) NOT NULL,
+city VARCHAR(32) NOT NULL,
+dept_id INT,
+PRIMARY KEY(eid),
+FOREIGN KEY(dept_id) REFERENCES dept(dept_id)
+);
+
+INSERT INTO employees
+VALUES
+(1,"Rajiv","BNG",101);
+
+
+INSERT INTO dept
+VALUES
+(102,"SALES","BNG",600000240),
+(103,"SALES","BNG",600004300),
+(104,"ADMIN","NOIDA",605400000),
+(105,"ADMIN","Gandhi Nagar",43540000)
+
+
+-- matched rows from both tables 
+select * from employee,dept
+where employees.dept_id So I am given the courage to have employee city and department given all the queries from whatever once you've done this one location order details. order amount customer idea order system so here customer idea reference references to I will set the data types and common user it means order IDs order IDs develop IDS order quantity so credit customer IDs or data or quantities quantity one quantity two begin large ID and mobile number in case you are storing the data security ID and the back are tender and description comments decimal salary and price so most we are not using decimal check in Postgres we call decimal data type so I was an example once you turn this one now I'm not giving any queries like now I'm giving the small assignment you have to give me finite=dept.dept_id
